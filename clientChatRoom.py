aa6b6ff06a9691e76a9d8b519aed38208b18527a
Carllos Eduardo Ferreira Lopes <52980718+carlloos@users.noreply.github.com>
2021-01-18 20:30:07 -0300

    Add files via upload

---
+++ clientChatRoom.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+# Escolhendo o nickname
+nickname = input("Escolha seu nickname: ")
+
+# Conectando ao servidor
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+# Espera por mensagens do servidor 
+def recebendo():
+    while True:
+        try:
+            # Recebendo mensagem do servidor
+            # Se a mensagem for 'NICK' envia o nickname
+            message = client.recv(1024).decode('UTF-8')
+            if message == 'NICK': #flag para enviar o nick
+                client.send(nickname.encode('UTF-8'))
+            else:
+                print(message)
+        except:
+            # Fecha a conexão em caso de erro
+            print("Um erro ocorreu!")
+            client.close()
+            break
+
+# Enviando mensagens
+def enviando():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('UTF-8'))
+
+#O cliente precisa ter duas threads ocorrendo ao mesmo tempo. Uma está recebendo mensagens do servidor
+#enquanto a outra está enviando mensagens
+# Inicia threads
+receive_thread = threading.Thread(target=recebendo)
+receive_thread.start()
+
+write_thread = threading.Thread(target=enviando)
+write_thread.start()
---
+++ serverChatRoom.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+
+
+host = '127.0.0.1'
+port = 55555
+
+# Iniciando o servidor
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lista com os clientes e com os nicks
+clients = []
+nicknames = []
+
+#Posta as mensagens para cada cliente na lista de clientes
+def postar(message):
+    for client in clients:
+        client.send(message)
+
+
+#Tenta enviar mensagem para os clientes ou lida com clientes saindo do terminal
+def mensagens(client):
+    while True:
+        try:
+            # Enviando a mesagem
+            message = client.recv(1024)
+            postar(message)
+
+        except:
+            # Caso o cliente feche o terminal, remover ele das listas
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            postar('{} saiu!'.format(nickname).encode('UTF-8'))
+            nicknames.remove(nickname)
+            break
+
+
+def main():
+    while True:
+        # Aceitando conexão
+        client, address = server.accept()
+        print("Conectado com {}".format(str(address)))
+
+        # Pedindo e guardando os nicks
+        client.send('NICK'.encode('UTF-8'))#flag para guardarmos o nick do cliente
+        nickname = client.recv(1024).decode('UTF-8')
+        clients.append(client)
+        nicknames.append(nickname)
+        postar("{} entrou no chat!".format(nickname).encode('UTF-8'))
+
+        # Começa a thread para o clinte
+        thread = threading.Thread(target=mensagens, args=(client,))
+        thread.start()
+
+main()
